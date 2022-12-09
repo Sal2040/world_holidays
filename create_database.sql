5bcad6405a79ba82f113da2cb4c411775e2547a9
Samer Alwan <alwan.samer24@gmail.com>
2022-12-09 11:47:40 +0100

    database design simplified

--- create_database.sql
+++ create_database.sql
@@ -8,29 +8,6 @@ CREATE TABLE IF NOT EXISTS holiday(
     date TIMESTAMP WITH TIME ZONE
 );
 
-CREATE TABLE IF NOT EXISTS country(
-    country_id CHAR(2) PRIMARY KEY,
-    name VARCHAR
-);
-
-CREATE TABLE IF NOT EXISTS state(
-    state_id CHAR(5) PRIMARY KEY,
-    abbrev  VARCHAR,
-    name VARCHAR,
-    county_id CHAR(2)
-);
-
-CREATE TABLE IF NOT EXISTS location(
-    location_id INT PRIMARY KEY,
-    name VARCHAR,
-    country_id CHAR(2)
-);
-
-CREATE TABLE IF NOT EXISTS type(
-    type_id INT PRIMARY KEY,
-    name VARCHAR
-);
-
 CREATE TABLE IF NOT EXISTS holiday_type(
     holiday_id INT REFERENCES holiday(holiday_id),
     type_id INT REFERENCES type(type_id),
