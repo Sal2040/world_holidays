cf3d468864aa273bf2cdf584d3a3420ceb4b4cdf
Samer Alwan <alwan.samer24@gmail.com>
2022-12-09 12:01:46 +0100

    database design fixed

--- create_database.sql
+++ create_database.sql
@@ -4,24 +4,24 @@ CREATE TABLE IF NOT EXISTS holiday(
     holiday_id INT PRIMARY KEY,
     name VARCHAR,
     description VARCHAR,
-    country_id CHAR(2),
+    country VARCHAR,
     date TIMESTAMP WITH TIME ZONE
 );
 
 CREATE TABLE IF NOT EXISTS holiday_type(
     holiday_id INT REFERENCES holiday(holiday_id),
-    type_id INT REFERENCES type(type_id),
-    PRIMARY KEY (holiday_id, type_id)
+    type VARCHAR,
+    PRIMARY KEY (holiday_id, type)
 );
 
 CREATE TABLE IF NOT EXISTS holiday_location(
     holiday_id INT REFERENCES holiday(holiday_id),
-    location_id INT REFERENCES location(location_id),
-    PRIMARY KEY (holiday_id, location_id)
+    location VARCHAR,
+    PRIMARY KEY (holiday_id, location)
 );
 
 CREATE TABLE IF NOT EXISTS holiday_state(
     holiday_id INT REFERENCES holiday(holiday_id),
-    state_id CHAR(5) REFERENCES state (state_id),
-    PRIMARY KEY (holiday_id, state_id)
+    state VARCHAR,
+    PRIMARY KEY (holiday_id, state)
 );
